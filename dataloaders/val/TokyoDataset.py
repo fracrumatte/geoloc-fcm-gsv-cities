498216a5662d50e5ae0c63d2b45f194e5db3bb06
FrancescaGeusa <geusa.francesca@gmail.com>
2024-06-11 12:58:16 +0200

    cambio path

--- dataloaders/val/TokyoDataset.py
+++ dataloaders/val/TokyoDataset.py
@@ -10,7 +10,7 @@ BASE_PATH = '/content/drive/MyDrive/geoloc_fcm/extracted_datasets/tokyo_xs/'
 
 
 # DATASET_ROOT = '/home/USER/work/datasets/Pittsburgh/' 
-DATASET_ROOT = 'geoloc-fcm-gsv-cities/datasets/SanFrancisco'  #questo è il mio path, va cambiato
+DATASET_ROOT = '/content/drive/MyDrive/geoloc_fcm/geoloc-fcm-gsv-cities/datasets/Tokyo'  #questo è il mio path, va cambiato
 #GT_ROOT = '/home/USER/work/gsv-cities/datasets/' # BECAREFUL, this is the ground truth that comes with GSV-Cities
 GT_ROOT = '/content/drive/MyDrive/geoloc_fcm/geoloc-fcm-gsv-cities/datasets/'    #questo è il mio path, va cambiato
 
